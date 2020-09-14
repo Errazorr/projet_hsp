a58ff881ad1dc9fa5f1e2e0813542a591941b712
ryanftne <ryan-fontaine@hotmail.fr>
2020-09-14 16:41:40 +0200

    class Manager pour inscription et connexion

--- manager/manager.php
+++ manager/manager.php
@@ -20,6 +20,40 @@ class Manager {
     ))
   }
 
+  public function Connexion($con){
+     $bdd = new PDO('mysql:host=localhost;dbname=hopital;charset=utf8','root','');
+     $r = $bdd->prepare('SELECT * FROM compte_patient WHERE nom = :nom AND prenom = :prenom AND date_naissance = :date_naissance AND mail = :mail AND adresse = :adresse AND mutuel = :mutuelle AND num_sec_soc = :num_sec_soc AND tv = :tv AND wifi = :wifi AND regime = :regime AND mdp = :mdp');
+     $r -> execute(array(
+       'nom' => $con->getNom(),
+       'prenom' => $con->getPrenom(),
+       'date_naissance' => $con->getDate_naissance(),
+       'mail' => $con->getMail(),
+       'adresse' => $con->getAdresse(),
+       'mutuelle' => $con->getMutuelle(),
+       'num_sec_soc' => $con->getNum_sec_soc(),
+       'tv' => $con->getTv(),
+       'wifi' => $con->getWifi(),
+       'regime' => $con->getRegime(),
+       'mdp' => $con->getMdp()
+     ));
+    $donne=$r->fetch();
 
-}
+    if ($donne['nom'] == $con->getNom() && $donne['prenom'] == $con->getPrenom() && $donne['mdp'] == $con->getMdp()) {
+      $_SESSION['id'] = $donne['id'];
+      $_SESSION['nom'] = $donne['nom'];
+      $_SESSION['prenom'] = $donne['prenom'];
+      if ($_SESSION['role'] == admin){
+       header('location: ../page_connexion.php');
+     }
+     else {
+     header('location: ../accueilconnexion.php');
+     }
+   }
+   else {
+     echo 'erreur';
+   }
+
+ }
+
+  }
  ?>
