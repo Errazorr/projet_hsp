ba7f2fc871b042ae17b8f0f5cf39d1494bf92b9b
Errazorr <n.goncalves@lprs.fr>
2020-09-21 17:03:52 +0200

    Finalisation du traitement pour le mot de passe oublié

--- traitement/mdp_oublie.php
+++ traitement/mdp_oublie.php
@@ -19,8 +19,11 @@ else{
   $resultat = $req->fetch();
 
   if($resultat){
-    $req = $bdd->prepare('UPDATE compte_patient SET mdp=? WHERE nom=? AND num_sec_soc=?');
-    $req->execute(array(md5($_POST['new_password']), $_POST['nom'], $_POST['securite_sociale']));
+    $req = $bdd->prepare('UPDATE `compte` SET `mdp`=? WHERE `nom`=? AND `num_sec_soc`=?');
+    $update_request = $req->execute(array(md5($_POST['new_password']), $_POST['nom'], $_POST['securite_sociale']));
+    echo '<body onLoad="alert(\'Le mot de passe a bien été modifié\')">';
+
+    echo '<meta http-equiv="refresh" content="0;URL=../views/mdp_oublie.html">';
   }
   else{
     echo '<body onLoad="alert(\'Ce compte est introuvable\')">';
@@ -29,4 +32,5 @@ else{
   }
 }
 
- ?>
+var_dump($update_request);
+?>
