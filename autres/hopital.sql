2ce65d6b593a5bdd30cd45bae62f7a25f5d62e25
Errazorr <n.goncalves@lprs.fr>
2020-09-28 13:51:06 +0200

    MAJ BDD

--- autres/hopital.sql
+++ autres/hopital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 21 sep. 2020 à 12:39
+-- Généré le :  lun. 28 sep. 2020 à 11:50
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `hopital`
 --
+CREATE DATABASE IF NOT EXISTS `hopital` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `hopital`;
 
 -- --------------------------------------------------------
 
@@ -38,21 +40,77 @@ CREATE TABLE IF NOT EXISTS `compte` (
   `adresse` text,
   `mutuelle` varchar(50) DEFAULT NULL,
   `num_sec_soc` varchar(15) DEFAULT NULL,
-  `tv` tinyint(1) DEFAULT NULL,
-  `wifi` tinyint(1) DEFAULT NULL,
+  `option_chambre` varchar(10) DEFAULT NULL,
   `regime` text,
   `mdp` varchar(50) NOT NULL,
   `role` varchar(7) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `fk_patient` (`nom`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `compte`
 --
 
-INSERT INTO `compte` (`id`, `nom`, `prenom`, `date_naissance`, `mail`, `adresse`, `mutuelle`, `num_sec_soc`, `tv`, `wifi`, `regime`, `mdp`, `role`) VALUES
-(1, 'admin', 'admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '098f6bcd4621d373cade4e832627b4f6', 'admin');
+INSERT INTO `compte` (`id`, `nom`, `prenom`, `date_naissance`, `mail`, `adresse`, `mutuelle`, `num_sec_soc`, `option_chambre`, `regime`, `mdp`, `role`) VALUES
+(1, 'admin', 'admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '098f6bcd4621d373cade4e832627b4f6', 'admin'),
+(2, 'Kebiche', 'Killian', '27/06/2000', 'k.kebiche@gmail.com', '3 rue des potiers 93300 Aubervilliers', 'test', '152485963258745', 'Wifi et TV', NULL, '098f6bcd4621d373cade4e832627b4f6', 'patient');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `medecin`
+--
+
+DROP TABLE IF EXISTS `medecin`;
+CREATE TABLE IF NOT EXISTS `medecin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `lieu` varchar(50) NOT NULL,
+  `specialite` varchar(50) NOT NULL,
+  `identifiant` varchar(50) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `fk_medecin` (`nom`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `medecin`
+--
+
+INSERT INTO `medecin` (`id`, `nom`, `lieu`, `specialite`, `identifiant`, `mdp`) VALUES
+(1, 'Aggoun', 'Aubervilliers', 'Neurologie', 'Lucario', 'c1078f95440f31e9c88138226b1af484'),
+(2, 'Guo', 'Bourget', 'Tabacologie', 'az', 'a9d3b34800d4283ed34b2bbbeb443a77'),
+(3, 'Tang', 'Aulnay-sous-bois', 'Gynécologie', 'tang', '87db92494758df7d0f0a4f39162a48f4'),
+(4, 'Birba', 'Dugny', 'Cardiologie', 'Dr.CapriSun', 'be6e0613371881bfe15b363bf5d00c20');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_patient` varchar(50) NOT NULL,
+  `nom_medecin` varchar(50) NOT NULL,
+  `rais_consult` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_patient` (`nom_patient`),
+  KEY `fk_medecin` (`nom_medecin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `fk_medecin` FOREIGN KEY (`nom_medecin`) REFERENCES `medecin` (`nom`),
+  ADD CONSTRAINT `fk_patient` FOREIGN KEY (`nom_patient`) REFERENCES `compte` (`nom`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
