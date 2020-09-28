ea82e82ff32782437d2ba63771aa44af167185a4
ryanftne <ryan-fontaine@hotmail.fr>
2020-09-28 14:02:21 +0200

    création de la vue et du traitement pour inscription

---
+++ traitement/inscription.php
@@ -0,0 +1,12 @@
+<?php
+
+require('../manager/manager.php');
+require('../model/inscription_class.php');
+
+$inscription = new inscription($_POST['nom'], $_POST['prenom'], $_POST['date_naissance'], $_POST['mail'], $_POST['adresse'], $_POST['mutuelle'], $_POST['num_sec_soc'], $_POST['option_chambre'], $_POST['regime'], $_POST['mdp']);
+
+$manager = new Manager();
+$manager->Inscription($inscription);
+var_dump($inscription);
+
+ ?>
