58063822ad238e7092a32f9ef1d95dd47aa6a384
Errazorr <n.goncalves@lprs.fr>
2020-10-05 17:08:30 +0200

    merge conflict 3

---
+++ manager/method.php
@@ -0,0 +1,104 @@
+<?php
+
+
+//Test de connexion à la bdd //
+try{
+
+}
+catch (Exception $e){
+  die('Erreur:'.$e->getMessage());
+}
+
+class Method {
+
+  public function Inscription($ins){
+    $bdd= new PDO('mysql:host=localhost;dbname=hopital; charset=utf8','root','');
+    $req = $bdd->prepare('SELECT * FROM compte WHERE nom=? AND prenom=? AND mail=?');
+    $req->execute(array($ins->getNom(), $ins->getPrenom(), $ins->getMail()));
+    $donnees= $req->fetch();
+
+    if ($donnees) {
+      echo '<body onLoad="alert(\'Ce compte existe déjà\')">';
+
+      echo '<meta http-equiv="refresh" content="0;URL=../views/inscription.html">';
+    }
+    else{
+      $r = $bdd->prepare('INSERT INTO compte (nom, prenom, date_naissance, mail, adresse, mutuelle, num_sec_soc, option_chambre, regime, mdp, role) VALUES (:nom, :prenom, :date_naissance, :mail, :adresse, :mutuelle, :num_sec_soc, :option_chambre, :regime, :mdp, :role)');
+      $r ->execute(array(
+        'nom' => $ins->getNom(),
+        'prenom' => $ins->getPrenom(),
+        'date_naissance' => $ins->getDateNaissance(),
+        'mail' => $ins->getMail(),
+        'adresse' => $ins->getAdresse(),
+        'mutuelle' => $ins->getMutuelle(),
+        'num_sec_soc' => $ins->getNumSecSoc(),
+        'option_chambre' => $ins->getOptionChambre(),
+        'regime' => $ins->getRegime(),
+        'mdp' => md5($ins->getMdp()),
+        'role' => 'patient'
+      ));
+    }
+  }
+
+  public function connexion($connexion){
+    $bdd= new PDO('mysql:host=localhost;dbname=hopital; charset=utf8','root','');
+    // Sélectionne les informations de la table compte en fonction de l'adresse mail //
+    $req = $bdd->prepare('SELECT * FROM compte WHERE mail=?');
+    $req->execute(array($connexion->getMail()));
+    $donnees= $req->fetch();
+    // Si la rêquette s'execute alors on redirige vers la page d'accueil //
+    if ($donnees['mail'] == $connexion->getMail() AND $donnees['mdp'] == md5($connexion->getMdp())) {
+      $_SESSION['nom'] = $donnees['nom'];
+      $_SESSION['prenom'] = $donnees['prenom'];
+      $_SESSION['mail'] = $connexion->getMail();
+      header('Location: ../landing.php');
+    }
+
+    else{
+      // Si non on affiche une erreur et on redirige vers la page connexion//
+      echo '<body onLoad="alert(\'Mail ou Mot de passe incorrect\')">';
+
+      echo '<meta http-equiv="refresh" content="0;URL=../View/Connexion.php">';
+    }
+  }
+
+ public function Reservation($rdv){
+   $bdd= new PDO('mysql:host=localhost;dbname=hopital; charset=utf8','root','');
+   $date_jour = date('Y-m-d');
+   $date_consult = $rdv->getDateConsult();
+
+   if ($date_consult < $date_jour) {
+     echo '<body onLoad="alert(\'Date invalide\')">';
+
+     echo '<meta http-equiv="refresh" content="0;URL=../views/prise_rdv.php">';
+   }
+
+   else{
+     $res = $bdd->prepare('SELECT * FROM compte WHERE nom = :nom AND prenom = :prenom');
+     $res ->execute(array(
+       'nom' => $rdv->getNomPatient(),
+       'prenom' => $rdv->getPrenomPatient()
+     ));
+
+     $patient = $res->fetch();
+
+     if ($patient) {
+       $res = $bdd->prepare('INSERT INTO reservation (nom_patient, nom_medecin, rais_consult, date_consult) VALUES (:nom_patient, :nom_medecin, :rais_consult, :date_consult)');
+       $res ->execute(array(
+         'nom_patient' => $rdv->getNomPatient(),
+         'nom_medecin' => $rdv->getNomMedecin(),
+         'rais_consult' => $rdv->getRaisonConsult(),
+         'date_consult' => $rdv->getDateConsult()
+       ));
+     }
+     else{
+       echo '<body onLoad="alert(\'Patient introuvable\')">';
+
+       echo '<meta http-equiv="refresh" content="0;URL=../views/prise_rdv.php">';
+     }
+
+  }
+ }
+
+}
+ ?>
---
+++ model/user_class.php
@@ -0,0 +1,92 @@
+<?php
+
+class User {
+// classe user reprenant les informations du manager, formulaire, model//
+
+private $nom;
+private $prenom;
+private $date_naissance;
+private $mail;
+private $adresse;
+private $mutuelle;
+private $num_sec_soc;
+private $option_chambre;
+private $regime;
+private $mdp;
+private $role;
+
+  public function __construct(array $donnees){
+    $this->hydrate($donnees);
+  }
+
+  public function hydrate (array $donnees){
+    foreach ($donnees as $key => $value) {
+      $method='set'.ucfirst($key);
+      if(method_exists($this, $method)){
+        $this->$method($value);
+      }
+    }
+  }
+
+  //Déclaration de l'ensemble des setter //
+
+  public function setNom ($nom){
+    $this->_nom = $nom;
+  }
+
+  public function setPrenom ($prenom){
+    $this->_prenom = $prenom;
+  }
+
+  public function setDateNaissance ($date_naissance){
+    $this->_date_naissance = $date_naissance;
+  }
+
+  public function setMail ($mail){
+    $this->_mail = $mail;
+  }
+
+  public function setAdresse ($adresse){
+    $this->_adresse = $adresse;
+  }
+
+  public function setMutuelle ($mutuelle){
+    $this->_mutuelle = $mutuelle;
+  }
+
+  public function setNumSecSoc ($num_sec_soc){
+    $this->_num_sec_soc = $num_sec_soc;
+  }
+
+  public function setOptionChambre ($option_chambre){
+    $this->_option_chambre = $option_chambre;
+  }
+
+  public function setRegime ($regime){
+    $this->_regime = $regime;
+  }
+
+  public function setMdp ($mdp){
+    $this->_mdp = $mdp;
+  }
+
+  public function setRole ($role){
+    $this->_role = $role;
+  }
+
+  //Déclaration de l'ensemble des getters //
+
+  public function getNom(){return $this->_nom;}
+  public function getPrenom(){return $this->_prenom;}
+  public function getDateNaissance(){return $this->_date_naissance;}
+  public function getMail(){return $this->_mail;}
+  public function getAdresse(){return $this->_adresse;}
+  public function getMutuelle(){return $this->_mutuelle;}
+  public function getNumSecSoc(){return $this->_num_sec_soc;}
+  public function getOptionChambre(){return $this->_option_chambre;}
+  public function getRegime(){return $this->_regime;}
+  public function getMdp(){return $this->_mdp;}
+  public function getRole(){return $this->_role;}
+}
+
+?>
