9821702439ebbedcb0cc16e0a651d09b599bfb85
Errazorr <n.goncalves@lprs.fr>
2020-11-09 14:21:42 +0100

    Ajout commentaires sur modif mdp

--- modification_mdp.php
+++ modification_mdp.php
@@ -1,35 +1,37 @@
 <?php
+//CONNEXION A LA BDD
   $bdd= new PDO('mysql:host=localhost;dbname=hopital; charset=utf8','root','');
 session_start();
 
+//SI IL Y A UN MAIL
 if(isset($_POST['mail'])) {
+  //SI LE MOT DE PASSE ET LA CONFIRMATION SONT LES MÊMES
   if($_POST['mdp'] == $_POST['mdp2']) {
-$reponse=$bdd->prepare('SELECT * FROM compte WHERE mail = ? ');
+    //REQUETE SQL
+    $reponse=$bdd->prepare('SELECT * FROM compte WHERE mail = ? ');
 
-$reponse->execute(array(
-  $_POST['mail']
-));
+    $reponse->execute(array( $_POST['mail'] ));
 
-$donnee=$reponse->fetch();
+    $donnee=$reponse->fetch();
 
-if ($donnee)
-{
-$reponseCompte=$bdd->prepare('UPDATE compte SET mdp = ? WHERE mail = ? ');
+    //SIL IL TROUVE QUELQUE CHOSE DANS LA BDD
+    if ($donnee){
+      //UPDATE LE MDP
+      $reponseCompte=$bdd->prepare('UPDATE compte SET mdp = ? WHERE mail = ? ');
 
-$reponseCompte->execute(array(
-                  md5($_POST['mdp']),
-                  $_POST['mail']));
+      $reponseCompte->execute(array( md5($_POST['mdp']),
+                                    $_POST['mail']));
 
-header("location: views/connexion.php");
+      header("location: views/connexion.php");
 
-$reponseCompte->closeCursor();
+      $reponseCompte->closeCursor();
 
-if($reponseCompte) {
-  echo 'Votre mot de passe a été changé';
-}
+      if($reponseCompte) {
+        echo 'Votre mot de passe a été changé';
+      }
 
-}
-}
+    }
+  }
 }
 ?>
 
@@ -41,29 +43,29 @@ if($reponseCompte) {
     <title>Modification du mot de passe</title>
   </head>
   <body>
-<div align="center">
-  <h1> Récupérez votre mot de passe </h1>
-    <form class="" action="" method="post">
+    <div align="center">
+      <h1> Récupérez votre mot de passe </h1>
+      <form class="" action="" method="post">
       <table border="0" cellpadding="5" cellspacing="15">
         <tr>
-<td> <label for="mail">Votre adresse mail :</label> </td>
-<td>   <input type="mail" name="mail" placeholder="exemple@exemple.fr" /> </td>
+          <td> <label for="mail">Votre adresse mail :</label> </td>
+          <td> <input type="mail" name="mail" placeholder="exemple@exemple.fr" /> </td>
       </tr>
-<br><br>
-<tr>
-<td> <label for="mdp">Nouveau mot de passe :</label> </td>
-<td>   <input type="password" name="mdp" placeholder="motdepasse" /> </td>
-</tr>
-<br><br>
-<tr>
-<td> <label for="mdp2">Confirmation mot de passe :</label> </td>
-<td>   <input type="password" name="mdp2" placeholder="confirmation" /> </td>
-</tr>
-<br><br>
-<tr>
-<td align="center" colspan="2">
-<input type="submit" name="valider_email" value="Valider">
-</td>
+      <br><br>
+      <tr>
+        <td> <label for="mdp">Nouveau mot de passe :</label> </td>
+        <td>   <input type="password" name="mdp" placeholder="motdepasse" /> </td>
+      </tr>
+      <br><br>
+      <tr>
+        <td> <label for="mdp2">Confirmation mot de passe :</label> </td>
+        <td>   <input type="password" name="mdp2" placeholder="confirmation" /> </td>
+      </tr>
+      <br><br>
+      <tr>
+        <td align="center" colspan="2">
+          <input type="submit" name="valider_email" value="Valider">
+        </td>
     </form>
   </body>
 </html>
