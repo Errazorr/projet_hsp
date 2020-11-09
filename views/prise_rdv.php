981a239e2d7fc7f08c854cbd5edc627365bfe8ef
Errazorr <n.goncalves@lprs.fr>
2020-11-09 14:40:35 +0100

    Ajout des demi heures pour prise de rdv

--- views/prise_rdv.php
+++ views/prise_rdv.php
@@ -127,7 +127,7 @@
 
 
                 											<?php
-                											// Sélection des films //
+                											// Sélection des médecins //
                 											$req = $bdd->query('SELECT nom FROM medecin');
                 									    $donnees= $req->fetchall();
 
@@ -148,38 +148,9 @@
                                     <?php
                                       for ($i=8; $i < 22 ; $i++) {
                                         echo '<option>'.$i.':00'.'</option>';
-                                        //echo '<option>'.$i.':30'.'</option>';
+                                        echo '<option>'.$i.':30'.'</option>';
                                       }
                                     ?>
-                									  <!--<option>08:00</option>
-                                        <option>08:30</option>
-                                        <option>09:00</option>
-                                        <option>09:30</option>
-                                        <option>10:00</option>
-                                        <option>10:30</option>
-                                        <option>11:00</option>
-                                        <option>11:30</option>
-                                        <option>12:00</option>
-                                        <option>12:30</option>
-                                        <option>13:00</option>
-                                        <option>13:30</option>
-                                        <option>14:00</option>
-                                        <option>14:30</option>
-                                        <option>15:00</option>
-                                        <option>15:30</option>
-                                        <option>16:00</option>
-                                        <option>16:30</option>
-                                        <option>17:00</option>
-                                        <option>17:30</option>
-                                        <option>18:00</option>
-                                        <option>18:30</option>
-                                        <option>19:00</option>
-                                        <option>19:30</option>
-                                        <option>20:00</option>
-                                        <option>20:30</option>
-                                        <option>21:00</option>
-                                        <option>21:30</option>
-                                        <option>22:00</option>-->
                 									</select>
                                 </div>
                                 <div class="form-group col-md-6">
