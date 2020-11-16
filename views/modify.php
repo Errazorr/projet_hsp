e18e54ec39b1a9625c9b5b9e6f6160c094037c67
Errazorr <n.goncalves@lprs.fr>
2020-11-16 14:33:02 +0100

    Test template sur modify

--- views/modify.php
+++ views/modify.php
@@ -35,41 +35,98 @@ if (isset($_GET['id']) AND !empty($_GET['id']))  // recup url
 }
  ?>
 
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
+ <head>
+     <!-- Required meta tags -->
      <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <title>Modification d'un compte</title>
      <link rel="icon" href="../img/logo3icon.png">
      <title>Modifier membre - Administration</title>
-   </head>
-   <body>
 
-<div align="center">
+      <!-- breadcrumb start-->
+      <section class="breadcrumb_part breadcrumb_bg">
+         <div class="container">
+             <div class="row">
+                 <div class="col-lg-12">
+                     <div class="breadcrumb_iner">
+                         <div class="breadcrumb_iner_item">
+                             <h2>Modification</h2>
 
-     <form action="" method="post">
-       <label for="nom">Nom : </label>
-       <input type="text" name="nom" value="<?= $whoiam['nom']; ?>"><br/><br/>
-       <label for="prenom">Prénom : </label>
-       <input type="text" name="prenom" value="<?= $whoiam['prenom']; ?>"><br/><br/>
-       <label for="date_naissance">Date de naissance : </label>
-       <input type="date" name="date_naissance" value="<?= $whoiam['date_naissance']; ?>"><br/><br/>
-       <label for="mail">Mail : </label>
-       <input type="email" name="mail" value="<?= $whoiam['mail']; ?>"><br/><br/>
-       <label for="adresse">Adresse : </label>
-       <input type="text" name="adresse" value="<?= $whoiam['adresse']; ?>"><br/><br/>
-       <label for="mutuelle">Mutuelle : </label>
-       <input type="text" name="mutuelle" value="<?= $whoiam['mutuelle']; ?>"><br/><br/>
-       <label for="num_sec_soc">Num Sec Soc : </label>
-       <input type="tel" maxlength="15" name="num_sec_soc" value="<?= $whoiam['num_sec_soc']; ?>"><br/><br/>
-       <label for="option_chambre">Option : </label>
-       <input type="text" name="option_chambre" value="<?= $whoiam['option_chambre']; ?>"><br/><br/>
-       <label for="regime">Regime : </label>
-       <input type="text" name="regime" value="<?= $whoiam['regime']; ?>"><br/><br/>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
 
-       <input type="submit" name="ok_modifier_membre" value="Modifier">
+     <section class="regervation_part section_padding">
+         <div class="container">
+             <div class="row align-items-center regervation_content">
+                 <div class="col-lg-12">
 
-     </form>
-</div>
-   </body>
+                    <div class="regervation_part_iner">
+                         <form action="" method="post">
+                           <center>
+
+                            <h2>Vous désirez changer vos données? </h2> </center>
+                             <div class="form-row">
+                                 <div class="form-group col-md-6">
+                                   <label for="nom">Nom : </label>
+                                   <input type="text" name="nom" value="<?= $whoiam['nom']; ?>"><br/><br/>
+                                 </div>
+                                 <div class="form-group col-md-6">
+                                   <label for="prenom">Prénom : </label>
+                                   <input type="text" name="prenom" value="<?= $whoiam['prenom']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="date_naissance">Date de naissance : </label>
+                                   <input type="date" name="date_naissance" value="<?= $whoiam['date_naissance']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="mail">Mail : </label>
+                                   <input type="email" name="mail" value="<?= $whoiam['mail']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="adresse">Adresse : </label>
+                                   <input type="text" name="adresse" value="<?= $whoiam['adresse']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="mutuelle">Mutuelle : </label>
+                                   <input type="text" name="mutuelle" value="<?= $whoiam['mutuelle']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="num_sec_soc">Num Sec Soc : </label>
+                                   <input type="tel" maxlength="15" name="num_sec_soc" value="<?= $whoiam['num_sec_soc']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="option_chambre">Option : </label>
+                                   <input type="text" name="option_chambre" value="<?= $whoiam['option_chambre']; ?>"><br/><br/></div>
+                                 <div class="form-group col-md-6">
+                                   <label for="regime">Regime : </label>
+                                   <input type="text" name="regime" value="<?= $whoiam['regime']; ?>"><br/><br/></div>
+ <br/>
+   </br/>
+
+                             </div>
+                             <div class="regerv_btn"><center>
+
+
+                                 <input type="submit" name="ok_modifier_membre" value="Modifier">
+                                 <br/>  </center>
+
+                               </br/>		<center>
+ <br/>
+
+ </br/>
+                             </div>
+
+
+                         </form>
+                     </div>
+
+                 </div>
+
+             </div>
+         </div>
+     </section>
+     <!--::regervation_part end::-->
+
+     <?php
+     require_once('footer.php');
+      ?>
+
  </html>
