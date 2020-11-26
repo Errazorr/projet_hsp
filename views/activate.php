9336c83ac8ad9b10481cb06598583976bde1eeee
ryanftne <ryan-fontaine@hotmail.fr>
2020-11-26 13:20:55 +0100

    Possibilité de désapprouver les comptes (admin)

--- views/activate.php
+++ views/activate.php
@@ -11,10 +11,20 @@ if(isset($_GET['type']) AND $_GET['type'] == 'membre') {
     $req = $bdd->prepare('DELETE FROM patient WHERE id = ?');
     $req->execute(array($supprime));
  }
+ if(isset($_GET['desaprouve']) AND !empty($_GET['desaprouve'])) {
+    $confirme = (int) $_GET['desaprouve'];
+    $req = $bdd->prepare('UPDATE patient SET confirme = 0 WHERE id = ?');
+    $req->execute(array($confirme));
+ }
 } elseif(isset($_GET['type']) AND $_GET['type'] == 'commentaire') {
  if(isset($_GET['approuve']) AND !empty($_GET['approuve'])) {
     $approuve = (int) $_GET['approuve'];
     $req = $bdd->prepare('UPDATE medecin SET approuve = 1 WHERE id = ?');
+    $req->execute(array($approuve));
+ }
+ if(isset($_GET['desaprouvemed']) AND !empty($_GET['desaprouvemed'])) {
+    $approuve = (int) $_GET['desaprouvemed'];
+    $req = $bdd->prepare('UPDATE medecin SET approuve = 0 WHERE id = ?');
     $req->execute(array($approuve));
  }
  if(isset($_GET['supprime']) AND !empty($_GET['supprime'])) {
@@ -23,8 +33,8 @@ if(isset($_GET['type']) AND $_GET['type'] == 'membre') {
     $req->execute(array($supprime));
  }
 }
-$membres = $bdd->query('SELECT * FROM patient ORDER BY id DESC LIMIT 0,5');
-$commentaires = $bdd->query('SELECT * FROM medecin ORDER BY id DESC LIMIT 0,5');
+$membres = $bdd->query('SELECT * FROM patient ORDER BY id DESC');
+$commentaires = $bdd->query('SELECT * FROM medecin ORDER BY id DESC');
 ?>
 <!DOCTYPE html>
 <html>
@@ -64,18 +74,27 @@ $commentaires = $bdd->query('SELECT * FROM medecin ORDER BY id DESC LIMIT 0,5');
           <ul>
              <?php while($m = $membres->fetch()) { ?>
              <li><?= $m['id'] ?> : <?= $m['nom'] ?> - <?= $m['prenom'] ?> - Confirme : <?= $m['confirme'] ?> :<?php if($m['confirme'] == 0) { ?>
-               - <a href="activate.php?type=membre&confirme=<?= $m['id'] ?>" style="text-decoration: none; color: #3ED400;">Confirmer</a><?php } ?> - <a href="activate.php?type=membre&supprime=<?= $m['id'] ?>" style="text-decoration: none; color: #b30000;">Supprimer</a></li>
+               - <a href="activate.php?type=membre&confirme=<?= $m['id'] ?>" style="text-decoration: none; color: #3ED400;">Confirmer</a><?php } ?> - <a href="activate.php?type=membre&supprime=<?= $m['id'] ?>" style="text-decoration: none; color: #b30000;">Supprimer</a>
+               <?php if($m['confirme'] == 1) { ?>
+                 - <a href="activate.php?type=membre&desaprouve=<?= $m['id'] ?>" style="text-decoration: none; color: #FFA500;">Désapprouver</a><?php } ?>
+             </li>
              <?php } ?>
           </ul>
           <br /><br />
           <ul>
              <?php while($c = $commentaires->fetch()) { ?>
-             <li><?= $c['id'] ?> : <?= $c['nom'] ?> : <?= $c['specialite'] ?><?php if($c['approuve'] == 0) { ?> - <a href="activate.php?type=commentaire&approuve=<?= $c['id'] ?>">Approuver</a><?php } ?> -
-               <a href="activate.php?type=commentaire&supprime=<?= $c['id'] ?>" style="text-decoration: none; color: #b30000;">Supprimer</a></li>
+             <li><?= $c['id'] ?> : <?= $c['nom'] ?> : <?= $c['specialite'] ?> - Confirme : <?= $c['approuve'] ?> :<?php if($c['approuve'] == 0) { ?> - <a href="activate.php?type=commentaire&approuve=<?= $c['id'] ?>" style="text-decoration: none; color: #3ED400;">Approuver</a><?php } ?> -
+               <a href="activate.php?type=commentaire&supprime=<?= $c['id'] ?>" style="text-decoration: none; color: #b30000;">Supprimer</a>
+<?php if($c['approuve'] == 1) { ?> - <a href="activate.php?type=commentaire&desaprouvemed=<?= $c['id'] ?>" style="text-decoration: none; color: #FFA500;">Désapprouver</a><?php } ?>
+             </li>
              <?php } ?>
           </ul>
-
+          <br><br>
+          <center>
+                  <a href="../page_index.php" >Retour<span
+                      ></span></a>
  				</form>
+
  			</div>
  		</div>
 
