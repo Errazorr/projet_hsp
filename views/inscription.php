d231f0bb855cfabab261b7ad86ee41cc3abeb038
Errazorr <trafalgar.nathan@gmail.com>
2020-11-30 16:42:38 +0100

    police inscription

--- views/inscription.php
+++ views/inscription.php
@@ -37,39 +37,39 @@
                         <form action="../traitement/inscription.php" method="post">
                           <center>
 
-                           <h2>Vous désirez prendre un rendez-vous ? Inscrivez-vous : </h2> </center>
+                           <h2 style="font-family: Arial, sans-serif">Vous désirez prendre un rendez-vous ? Inscrivez-vous : </h2> </center>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="nom" placeholder="Votre nom : ">
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="nom" placeholder="Votre nom : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="prenom" placeholder="Votre prénom : ">
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="prenom" placeholder="Votre prénom : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="date" class="form-control" name="date_naissance" placeholder="Votre date de naissance : ">
+                                    <input style="font-family: Arial, sans-serif" type="date" class="form-control" name="date_naissance" placeholder="Votre date de naissance : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="email" class="form-control" name="mail" required placeholder="Votre mail : ">
+                                    <input style="font-family: Arial, sans-serif" type="email" class="form-control" name="mail" required placeholder="Votre mail : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="adresse" placeholder="Votre adresse : ">
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="adresse" placeholder="Votre adresse : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="mutuelle" placeholder="Votre mutuelle : ">
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="mutuelle" placeholder="Votre mutuelle : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="tel" class="form-control" name="num_sec_soc"   maxlength="15" placeholder="Votre numéro de sécurité sociale : ">
+                                    <input style="font-family: Arial, sans-serif" type="tel" class="form-control" name="num_sec_soc"   maxlength="15" placeholder="Votre numéro de sécurité sociale : ">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="option_chambre" placeholder="Vos options de chambre (Wifi et/ou TV) : " maxlength="10">
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="option_chambre" placeholder="Vos options de chambre (Wifi et/ou TV) : " maxlength="10">
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="regime" placeholder="Votre régime : ">
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="regime" placeholder="Votre régime : ">
                                 </div>
 <br/>
   </br/>
                                 <div class="form-group col-md-6">
-                                    <input type="password" class="form-control" name="mdp" required
+                                    <input style="font-family: Arial, sans-serif" type="password" class="form-control" name="mdp" required
                                         placeholder="Mot de passe : ">
                                 </div>
 
@@ -77,14 +77,14 @@
                             <div class="regerv_btn"><center>
 
 
-                                <input type="submit" class="btn_1" name="btn" value="Créer son compte">
+                                <input style="font-family: Arial, sans-serif" type="submit" class="btn_1" name="btn" value="Créer son compte">
                                 <br/>  </center>
 
                               </br/>		<center>
 <br/>
 
 </br/>
-                                <a href="connexion.php" class="genric-btn danger-border circle arrow"> Déjà un compte ? Se connecter <span
+                                <a href="connexion.php" class="genric-btn danger-border circle arrow" style="font-family: Arial, sans-serif"> Déjà un compte ? Se connecter <span
                         						class="lnr lnr-arrow-right"></span></a></center>
                             </div>
 
