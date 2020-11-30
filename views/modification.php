d4237ca85ae4b2a10aee039355d4e2d2b0d3f603
Errazorr <trafalgar.nathan@gmail.com>
2020-11-30 16:40:44 +0100

    police modification

--- views/modification.php
+++ views/modification.php
@@ -29,7 +29,7 @@ $donnees= $rec->fetch();
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Modification du profil</title>
+    <title style="font-family: Arial, sans-serif">Modification du profil</title>
     <link rel="icon" href="../img/logo3icon.png">
 
      <!-- breadcrumb start-->
@@ -39,7 +39,7 @@ $donnees= $rec->fetch();
                 <div class="col-lg-12">
                     <div class="breadcrumb_iner">
                         <div class="breadcrumb_iner_item">
-                            <h2>Modification</h2>
+                            <h2 style="font-family: Arial, sans-serif">Modification</h2>
 
                         </div>
                     </div>
@@ -57,34 +57,34 @@ $donnees= $rec->fetch();
                         <form action="../traitement/modification.php" method="post">
                           <center>
 
-                           <h2>Vous désirez changer vos données? </h2> </center>
+                           <h2 style="font-family: Arial, sans-serif">Vous désirez changer vos données? </h2> </center>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="nom" placeholder="Votre nom : " <?php echo 'value='.'"'.$donnees["nom"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="nom" placeholder="Votre nom : " <?php echo 'value='.'"'.$donnees["nom"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="prenom" placeholder="Votre prénom : " <?php echo 'value='.'"'.$donnees["prenom"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="prenom" placeholder="Votre prénom : " <?php echo 'value='.'"'.$donnees["prenom"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="date" class="form-control" name="date_naissance" placeholder="Votre date de naissance : " <?php echo 'value='.'"'.$donnees["date_naissance"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="date" class="form-control" name="date_naissance" placeholder="Votre date de naissance : " <?php echo 'value='.'"'.$donnees["date_naissance"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="email" class="form-control" name="mail" placeholder="Votre mail : " <?php echo 'value='.'"'.$donnees["mail"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="email" class="form-control" name="mail" placeholder="Votre mail : " <?php echo 'value='.'"'.$donnees["mail"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="adresse" placeholder="Votre adresse : " <?php echo 'value='.'"'.$donnees["adresse"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="adresse" placeholder="Votre adresse : " <?php echo 'value='.'"'.$donnees["adresse"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="mutuelle" placeholder="Votre mutuelle : " <?php echo 'value='.'"'.$donnees["mutuelle"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="mutuelle" placeholder="Votre mutuelle : " <?php echo 'value='.'"'.$donnees["mutuelle"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="tel" class="form-control" name="num_sec_soc"   maxlength="15" placeholder="Votre numéro de sécurité sociale : " <?php echo 'value='.'"'.$donnees["num_sec_soc"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="tel" class="form-control" name="num_sec_soc"   maxlength="15" placeholder="Votre numéro de sécurité sociale : " <?php echo 'value='.'"'.$donnees["num_sec_soc"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="option_chambre" placeholder="Vos options de chambre (Wifi et/ou TV) : " maxlength="10" <?php echo 'value='.'"'.$donnees["option_chambre"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="option_chambre" placeholder="Vos options de chambre (Wifi et/ou TV) : " maxlength="10" <?php echo 'value='.'"'.$donnees["option_chambre"].'"'.''?>>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <input type="name" class="form-control" name="regime" placeholder="Votre régime : " <?php echo 'value='.'"'.$donnees["regime"].'"'.''?>>
+                                    <input style="font-family: Arial, sans-serif" type="name" class="form-control" name="regime" placeholder="Votre régime : " <?php echo 'value='.'"'.$donnees["regime"].'"'.''?>>
                                 </div>
 <br/>
   </br/>
@@ -93,14 +93,14 @@ $donnees= $rec->fetch();
                             <div class="regerv_btn"><center>
 
 
-                                <input type="submit" class="btn_1" name="btn" value="Modifier son compte">
+                                <input type="submit" class="btn_1" name="btn" value="Modifier son compte" style="font-family: Arial, sans-serif">
                                 <br/>  </center>
 
                               </br/>		<center>
 <br/>
 
 </br/>
-                                <a href="../forget_password.php" class="genric-btn danger-border circle arrow"> Modifier son mot de passe? <span
+                                <a href="../forget_password.php" class="genric-btn danger-border circle arrow" style="font-family: Arial, sans-serif"> Modifier son mot de passe? <span
                         						class="lnr lnr-arrow-right"></span></a></center>
                             </div>
 
