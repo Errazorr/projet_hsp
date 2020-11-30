95fd08b788be50028bfbca1eeb056dab599ed34b
Errazorr <trafalgar.nathan@gmail.com>
2020-11-30 16:29:57 +0100

    police modify

--- views/modify.php
+++ views/modify.php
@@ -41,7 +41,7 @@ if (isset($_GET['id']) AND !empty($_GET['id']))  // recup url
  <html>
  	<head>
  		<meta charset="utf-8">
- 		<title>RegistrationForm_v7 by Colorlib</title>
+ 		<title style="font-family: Arial, sans-serif">RegistrationForm_v7 by Colorlib</title>
  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
  		<!-- MATERIAL DESIGN ICONIC FONT -->
@@ -56,56 +56,56 @@ if (isset($_GET['id']) AND !empty($_GET['id']))  // recup url
  		<div class="wrapper">
  			<div class="inner">
  				<form action="" method="post">
- 					<h3>Modifier le compte</h3>
- 					<p>Modifier le compte suivant : <strong><?= $whoiam['id']; ?></strong> - <strong><?= $whoiam['nom']; ?></strong>  <strong><?= $whoiam['prenom']; ?></strong></p>
+ 					<h3 style="font-family: Arial, sans-serif">Modifier le compte</h3>
+ 					<p style="font-family: Arial, sans-serif">Modifier le compte suivant : <strong><?= $whoiam['id']; ?></strong> - <strong><?= $whoiam['nom']; ?></strong>  <strong><?= $whoiam['prenom']; ?></strong></p>
  					<label class="form-group">
  						<input type="text" name="nom" class="form-control" value="<?= $whoiam['nom']; ?>" >
- 						<span>Nom</span>
+ 						<span style="font-family: Arial, sans-serif">Nom</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="text" name="prenom" class="form-control" value="<?= $whoiam['prenom']; ?>" >
- 						<span>Prénom</span>
+ 						<span style="font-family: Arial, sans-serif">Prénom</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="date" name="date_naissance" class="form-control" value="<?= $whoiam['date_naissance']; ?>" >
- 						<span>Date de naissance</span>
+ 						<span style="font-family: Arial, sans-serif">Date de naissance</span>
  						<span class="border"></span>
  					</label>
  					<label class="form-group">
  						<input type="mail" name="mail" class="form-control" value="<?= $whoiam['mail']; ?>">
- 						<span for="">Adresse mail</span>
+ 						<span for=" style="font-family: Arial, sans-serif"">Adresse mail</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="text" name="adresse" class="form-control" value="<?= $whoiam['adresse']; ?>" >
- 						<span>Adresse</span>
+ 						<span style="font-family: Arial, sans-serif">Adresse</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="text" name="mutuelle" class="form-control" value="<?= $whoiam['mutuelle']; ?>" >
- 						<span>Mutuelle</span>
+ 						<span style="font-family: Arial, sans-serif">Mutuelle</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="text" name="num_sec_soc" class="form-control"  maxlength="15" name="num_sec_soc" value="<?= $whoiam['num_sec_soc']; ?>" >
- 						<span>Numéro de sécurité sociale</span>
+ 						<span style="font-family: Arial, sans-serif">Numéro de sécurité sociale</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="text" name="option_chambre" class="form-control" value="<?= $whoiam['option_chambre']; ?>" >
- 						<span>Option de chambre</span>
+ 						<span style="font-family: Arial, sans-serif">Option de chambre</span>
  						<span class="border"></span>
  					</label>
           <label class="form-group">
  						<input type="text" name="regime" class="form-control" value="<?= $whoiam['regime']; ?>" >
- 						<span>Régime</span>
+ 						<span style="font-family: Arial, sans-serif">Régime</span>
  						<span class="border"></span>
  					</label>
  				<center>
 
-        	<button type="submit" name="ok_modifier_membre">Modifier
+        	<button type="submit" name="ok_modifier_membre" style="font-family: Arial, sans-serif">Modifier
 </center>
  					</button>
  				</form>
